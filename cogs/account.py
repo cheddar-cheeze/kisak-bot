182731a0cc50f3b85b1a6d7027ecb599642e37a0
cheddar-cheeze <cheddarcheeze@protonmail.com>
2018-06-13 19:37:46 -0500

    now with used accounts tracking
    Signed-off-by: cheddar-cheeze <cheddarcheeze@protonmail.com>

--- cogs/account.py
+++ cogs/account.py
@@ -4,8 +4,7 @@ from discord.ext import commands
 from cogs.constants import embed_color
 
 
-
-acc = json.load(open('/home/ubuntu/generator/accounts.cg.json'))
+path = '/home/cheddar-cheeze/kisak-bot/accounts.cg.json'
 
 class account():
     def __init__(self, bot):
@@ -14,17 +13,40 @@ class account():
     @commands.command(pass_context=True, no_pm=True)
     @commands.cooldown(1, 60, commands.BucketType.user)
     async def account(self, ctx):
-        await self.bot.delete_message(ctx.message)
-        login = acc["array"][1]["login"]
-        passwd = acc["array"][1]["password"]
+        with open(path, 'r+') as json_file:
+            out = json.load(json_file)
+            used = out["used"]
+            out["used"] = used + 1
+            json_file.seek(0)
+            json_file.write(json.dumps(out))
+            json_file.truncate()
+            json_file.close()
+        login = out["array"][1]["login"]
+        passwd = out["array"][1]["password"]
+        del out["array"][1]
+        try:
+            await self.bot.delete_message(ctx.message)
+        except:
+            pass
         embed = discord.Embed(title="Account Credentials", color=embed_color)
         embed.add_field(name="Login", value=login)
         embed.add_field(name="Password", value=passwd)
-        del acc["array"][1]
         try:
             await self.bot.send_message(ctx.message.author, embed=embed)
         except:
             pass
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def  geninfo(self, ctx):
+        with open(path, 'r+') as json_file:
+            out = json.load(json_file)
+            used = out["used"]
+            json_file.close()
+        amt = len(out["array"])
+        embed = discord.Embed(title="Account generator info", color=embed_color)
+        embed.add_field(name='Account Stock', value=str(amt) + " accounts")
+        embed.add_field(name='Accounts Used', value=str(used) + " accounts", inline=True)
+        await self.bot.say(embed=embed)
 
 
 
